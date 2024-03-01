ae25480baf79e3ad3bded1683cf33912abecdfad
Nipun <nipunkhivansara@gmail.com>
2024-02-29 23:31:12 -0800

    Integrated hugging face and added sql connection in sqldbconn.js

--- server/vectorstore.js
+++ server/vectorstore.js
@@ -1,13 +1,16 @@
 const { MemoryVectorStore } = require("langchain/vectorstores/memory");
+const { HuggingFaceInferenceEmbeddings } = require("@langchain/community/embeddings/hf");
+
 const { OpenAIEmbeddings } = require("@langchain/openai");
 const dotenv = require("dotenv");
 const {Document} = require("langchain/document");
 const RecursiveCharacterTextSplitter = require("langchain/text_splitter");
 const fs = require("fs");
+const {TextLoader} = require("langchain/document_loaders/fs/text");
 
 dotenv.config();
 
-let vectorStore = null; // Initialize processedData as null
+let vectorStore = null;
 
 const getTableSchemas = async (filePath) => {
   // Parse the file , separate content by ";" and add it to the list
@@ -16,26 +19,28 @@ const getTableSchemas = async (filePath) => {
   return tableSchemas;
 };
 
-
 async function loadDocuments(filePath) {
   const tableSchemas = await getTableSchemas(filePath);
   return tableSchemas;
 }
 
-
-// TODO: This should be created only once at the start of the session
 const createVectorStore = async (docOutput) => {
   let ids = docOutput.map((_, index) => ({ id: index + 1 }));
   return  await MemoryVectorStore.fromTexts(
     docOutput,
     ids,
-    new OpenAIEmbeddings()
+    new HuggingFaceInferenceEmbeddings({
+      apiKey: "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", 
+      model: "intfloat/e5-large"
+    })
   );
 }
+
 
 async function similaritySearch(vectorStore, query, k) {
   return await vectorStore.similaritySearch(query, k);
 }
+
 const processDocuments = async (filePath, query, k) => {
   try {
     // Process documents only if processedData is null (first time)
