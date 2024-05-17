d8e4991a9c92f4f660b2e22bb4c922978304345f
Nipun <nipunkhivansara@gmail.com>
2024-05-17 11:58:33 -0700

    Added connection tab

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,30 +1,70 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import Sidebar from '../Sidebar/Sidebar';
-import Appbar from '../Appbar/Appbar';
-import Performance from '../Performance/Performance';
-import NewNotebook from '../NewNotebook/NewNotebook';
-import SiteInfo from '../SiteInfo/SiteInfo';
-import Home from '../Home/Home';
+import React from "react";
+import { Box } from "@mui/material";
+import Sidebar from "../Sidebar/Sidebar";
+import Appbar from "../Appbar/Appbar";
+import Performance from "../Performance/Performance";
+import NewNotebook from "../NewNotebook/NewNotebook";
+import Connection from "../Connections/Connections";
+import SiteInfo from "../SiteInfo/SiteInfo";
+import Home from "../Home/Home";
+import { useState } from "react";
 
+// c
+const Dashboard = ({
+  open,
+  user,
+  logout,
+  handleDrawerToggle,
+  menuBarWidth,
+}) => {
+  console.log(user);
 
+  const [connections, setConnections] = useState(false);
 
-const Dashboard = ({ open, user,logout, handleDrawerToggle, menuBarWidth }) => {
-    console.log(user);
-    return (
-        <div>
-            {/* <Home />  */}
-            <Box sx={{ zIndex: 1, display: 'flex' }}>
-                <Appbar handleDrawerToggle={handleDrawerToggle} menuBarWidth={menuBarWidth} user={user} />
-                <Sidebar open={open} menuBarWidth={menuBarWidth} logout={logout} user={user}/>
-                <Box sx={{ marginLeft: '-50px', marginTop: '60px', width: `${1930 - menuBarWidth}px`, height: '94.4vh', bgcolor: '#222B3D' }}>
-                    <Performance />
-                    <NewNotebook />
-                    <SiteInfo open={open} />
-                </Box>
-            </Box>
-        </div>
-    )
-}
+  const toggleConnections = () => {
+    setConnections(!connections);
+  };
 
-export default Dashboard
+  // console.log(connections);
+
+  return (
+    <div>
+      {/* <Home />  */}
+      <Box sx={{ zIndex: 1, display: "flex" }}>
+        <Appbar
+          handleDrawerToggle={handleDrawerToggle}
+          menuBarWidth={menuBarWidth}
+          user={user}
+        />
+        <Sidebar
+          open={open}
+          menuBarWidth={menuBarWidth}
+          logout={logout}
+          user={user}
+          toggleConnections={toggleConnections}
+        />
+        <Box
+          sx={{
+            marginLeft: "-50px",
+            marginTop: "60px",
+            width: `${1930 - menuBarWidth}px`,
+            height: "94.4vh",
+            bgcolor: "#222B3D",
+          }}
+        >
+          {connections ? (
+            <Connection />
+          ) : (
+            <>
+              <Performance />
+              <NewNotebook />
+              <SiteInfo open={open} />
+            </>
+          )}
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Dashboard;
