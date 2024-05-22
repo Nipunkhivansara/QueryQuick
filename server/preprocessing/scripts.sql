c593810ee4635374b0e34d456b6b71a9ee65e4d9
Meet Merchant <meet.merchant4@gmail.com>
2024-05-22 15:25:54 -0700

    latest

--- server/preprocessing/scripts.sql
+++ server/preprocessing/scripts.sql
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE,
-    profile VARCHAR(500) NULL
+    profile VARCHAR(500) NULL,
     UNIQUE (id, email)
 );
 
