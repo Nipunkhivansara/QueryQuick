8f66cd586bcdf4ee6473e6b0616a0e40df30a362
Nipun <nipunkhivansara@gmail.com>
2024-05-22 18:45:13 -0700

    Logout button added

--- client/src/services/sqlservice.js
+++ client/src/services/sqlservice.js
@@ -1,18 +1,17 @@
 // services/getDataFromSql.js
 
-
-async function getDataFromSql({ query, database }) {
-
-    try {
-        console.log(`Querying database: ${database} with query: ${query}`)
-        const response = await fetch(`http://localhost:5000/sql?query=${query}&database=${database}`);
-        const data = await response.json();
-        return data;
-    }
-    catch (error) {
-        console.error('Error fetching data from SQL:', error.message);
-        throw error;
-    }
+async function getDataFromSql({ query, cellDatabase }) {
+  try {
+    console.log(`Querying database: ${cellDatabase} with query: ${query}`);
+    const response = await fetch(
+      `http://localhost:5000/sql?query=${query}&database=${cellDatabase}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data from SQL:", error.message);
+    throw error;
+  }
 }
 
 export default getDataFromSql;
