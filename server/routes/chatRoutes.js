e84235fd768d485e472a082e425e4a0c02e40051
aswinuci <saswin@uci.edu>
2024-05-26 16:46:17 -0700

    Updated back acc to main

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -22,8 +22,8 @@ router.post("/chat", async (req, res) => {
   try {
     console.log("Post request received");
     const { prompt } = req.body;
-    res.status(200).json({ msg: "select * from events;" });
-    /* const result = await processQuery(prompt, "sql");
+   /*  res.status(200).json({ msg: "select * from events;" }); */
+    const result = await processQuery(prompt, "sql");
     const x =
       "..." +
       "This is the schema of the tables of my sql database" +
@@ -53,7 +53,7 @@ router.post("/chat", async (req, res) => {
       .catch((error) => {
         console.error("Error:", error);
         res.status(500).send(error); // Send an error response back to the client
-      }); */
+      });
   } catch (err) {
     console.error("Error:", err);
     res.status(500).send(err); // Send an error response back to the client
